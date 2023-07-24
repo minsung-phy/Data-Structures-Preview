73fc0dff36eca7dde8bd42b5c6538b52ef2ebdc0
이민성 <133635509+minsung-phy@users.noreply.github.com>
2023-07-24 13:50:56 +0900

    Add files via upload

--- 2. Arrays And Structures/union.c
+++ 2. Arrays And Structures/union.c
@@ -17,7 +17,7 @@ typedef struct {
 } date;
 
 typedef struct {
-    enum tagField {female, male} sex; // 열거형
+    enum tagField {female, male} sex; // 열거형: enum 열거형이름 {상수 이름};
     union {
         int children;
         int bread;
@@ -32,14 +32,28 @@ typedef struct {
     sexType sexInfo;
 } humanBeing;
 
+typedef struct {
+    char data;
+    struct list *link; // link는 포인터 변수
+} list;
+
 int main(void) {
     humanBeing person1, person2;
     
     person1.sexInfo.sex = male;
-    person1.sexInfo.u.bread = FALSE;
+    person1.sexInfo.u.bread = FALSE; // FALSE = 0 이므로 bread는 int형이다
     
     person2.sexInfo.sex = female;
     person2.sexInfo.u.children = 4;
+    
+    list item1, item2, item3;
+    item1.data = 'a';
+    item2.data = 'b';
+    item3.data = 'c';
+    item1.link = item2.link = item3.link = NULL;
+    
+    item1.link = &item2;
+    item2.link = &item3;
     
     return 0;
 }
