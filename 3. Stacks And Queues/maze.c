b4af91722369e6aa6552ef3ee7f85131af917a67
이민성 <133635509+minsung-phy@users.noreply.github.com>
2023-07-26 10:47:39 +0900

    Add files via upload

--- 3. Stacks And Queues/maze.c
+++ 3. Stacks And Queues/maze.c
@@ -7,39 +7,69 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TRUE 1
 #define FALSE 0
 #define MAX_STACK_SIZE 100 // 스택의 최대 크기
-#define INVALID_KEY -1
+#define MAX_ROW 13
+#define MAX_COL 17
+#define EXIT_ROW 11
+#define EXIT_COL 15
+
+int maze[MAX_ROW][MAX_COL];
+int mark[MAX_ROW][MAX_COL];
 
 typedef struct {
     short int vert;
     short int horiz;
 } offsets;
+offsets moving[8]; // 각 방향에 대한 이동 배열
 
 typedef struct {
     short int row;
     short int col;
     short int dir;
 } element;
-
-offsets moving[8]; // 각 방향에 대한 이동 배열
+element stack[MAX_STACK_SIZE];
 
 int top = -1; // if top is -1, it means the stack is empty
 
-//nextRow = row + move[dir].vert;
-//nextCol = col + move[dir].horiz;
-
-void path(void);
-void push(element item);
+void push(element);
 void stackFull(void);
 element pop(void);
 element stackEmpty(void);
+void path(void);
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void) {
+    int i, j;
+    
+    int maze_values[MAX_ROW][MAX_COL] = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}, {1,0,1,0,0,0,1,1,0,0,0,1,1,1,1,1,1}, {1,1,0,0,0,1,1,0,1,1,1,0,0,1,1,1,1}, {1,0,1,1,0,0,0,0,1,1,1,1,0,0,1,1,1}, {1,1,1,0,1,1,1,1,0,1,1,0,1,1,0,0,1}, {1,1,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1}, {1,0,0,1,1,0,1,1,1,0,1,0,0,1,0,1,1}, {1,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1}, {1,0,0,1,1,0,1,1,0,1,1,1,1,1,0,1,1}, {1,1,1,0,0,0,1,1,0,1,1,0,0,0,0,0,1}, {1,0,0,1,1,1,1,1,0,0,0,1,1,1,1,0,1}, {1,0,1,0,0,1,1,1,1,1,0,1,1,1,1,0,1}, {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
+    
+    memcpy(maze, maze_values, sizeof(int) * MAX_ROW * MAX_COL);
+    
+    // initialize mark
+    for (i = 0; i < MAX_ROW; i++)
+    {
+        for (j = 0; j < MAX_COL; j++)
+        {
+            mark[i][j] = 0;
+        }
+    }
+    
+    // initialize moving
+    moving[0].vert = -1; moving[0].horiz = 0; // N
+    moving[1].vert = -1; moving[1].horiz = 1; // NE
+    moving[2].vert = 0; moving[2].horiz = 1; // E
+    moving[3].vert = 1; moving[3].horiz = 1; // SE
+    moving[4].vert = 1; moving[4].horiz = 0; // S
+    moving[5].vert = 1; moving[5].horiz = -1; // SW
+    moving[6].vert = 0; moving[6].horiz = -1; // W
+    moving[7].vert = -1; moving[7].horiz = -1; // NW
+    
+    // run path algorithm
+    path();
+    
     return 0;
 }
 
@@ -51,22 +81,22 @@ void path(void)
     top = 0;
     stack[0].row = 1;
     stack[0].col = 1;
-    stack[0].dir = 1;
+    stack[0].dir = 2;
     while (top > -1 && !found)
     {
-        position = pop(&top);
+        position = pop();
         row = position.row;
         col = position.col;
         dir = position.dir;
         while (dir < 8 && !found)
         { // dir 방향으로 이동
-            nextRow = row + move[dir].vert;
-            nextCol = col + move[dir].horiz;
+            nextRow = row + moving[dir].vert;
+            nextCol = col + moving[dir].horiz;
             if (nextRow == EXIT_ROW && nextCol == EXIT_COL)
                 found = TRUE;
             else if (!maze[nextRow][nextCol] && !mark[nextRow][nextCol])
             {
-                mark([nextRow][nextCol]) = 1;
+                mark[nextRow][nextCol] = 1;
                 position.row = row;
                 position.col = col;
                 position.dir = ++dir;
@@ -83,7 +113,7 @@ void path(void)
         printf("The path is:\n");
         printf("row col\n");
         for (i = 0; i <= top; i++)
-            printf("%2d%5d", stack[i].row, stack[i].col);
+            printf("%2d%5d\n ", stack[i].row, stack[i].col);
         printf("%2d%5d\n", row, col);
         printf("%2d%5d\n", EXIT_ROW, EXIT_COL);
     }
@@ -106,13 +136,12 @@ void stackFull(void)
 element pop(void)
 { // stack의 최상위 원소를 삭제하고 반환
     if (top == -1)
-        return stackEmpty(); // returns an error key
+        stackEmpty(); // returns an error key
     return stack[top--];
 }
 
 element stackEmpty(void)
 {
-    element e;
-    e.key = INVALID_KEY;
-    return e;
+    fprintf(stderr, "Stack is full, cannot add element");
+    exit(EXIT_FAILURE);
 }
